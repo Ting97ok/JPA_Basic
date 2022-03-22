593963672d95da2d41c2670f03eb44d73a906123
최대현 <89396424+Ting97ok@users.noreply.github.com>
2022-03-22 14:24:10 +0900

    고급 매핑

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 
 public class JpaMain {
@@ -16,7 +18,11 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
 
+            em.persist(book);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
